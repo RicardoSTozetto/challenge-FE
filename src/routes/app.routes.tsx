f4ab096b135e0bbaaaf5ed928eceebed279ee69e
Ricardo Schleder Tozetto <ricardo@optim.eng.br>
2021-05-31 10:28:45 -0300

    Limpeza no projeto

--- src/routes/app.routes.tsx
+++ src/routes/app.routes.tsx
@@ -3,7 +3,6 @@ import React from 'react'
 import { Switch, Route } from 'react-router-dom'
 
 import Layout from '../components/Layout';
-import Dashboard from '../pages/Dashboard'
 import List from '../pages/List'
 
 
